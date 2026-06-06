89252ee6730f470222f44eed3407d619690a14b6
vitaliksep <vitaliksep@yandex.ru>
2026-06-06 17:33:31 +0500

    Fix react-router config with ignoredRouteFiles

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,4 +3,7 @@ import type { Config } from '@react-router/dev/config';
 export default {
 	appDirectory: './src/app',
 	ssr: false,
+	buildDirectory: './build',
+	assetsBuildDirectory: './build/client',
+	ignoredRouteFiles: ['**/\.', '**/*.test.*', '**/__*', '**/.*'],
 } satisfies Config;
